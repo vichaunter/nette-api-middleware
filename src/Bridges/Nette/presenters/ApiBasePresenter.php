ad06e7bbdb9a3b25ecff9f8caa5384295e8d06f3
VicHaunter <vichaunter@gmail.com>
2019-03-10 21:25:25 +0100

    save changes

--- src/Bridges/Nette/presenters/ApiBasePresenter.php
+++ src/Bridges/Nette/presenters/ApiBasePresenter.php
@@ -8,24 +8,23 @@
 
 namespace VicHaunter\ApiMiddleware\Presenters;
 
+use Nette\Application\IPresenter;
 use Nette\Application\IResponse;
 use Nette\Application\Request;
-use Nette\Application\IPresenter;
 use Nette\Application\Responses\JsonResponse;
 use VicHaunter\ApiMiddleware\ApiApplicationRequest;
 
 class ApiBasePresenter implements IPresenter {
-    
-    /**
-    * @return IResponse
-    */
-   function run( Request $request ) {
-        $appRequest = new ApiApplicationRequest($request);
-
-        $handler = 'action' . ucfirst( $request->getParameter('action') );
-        $response = $this->$handler($appRequest);
-
-        return new JsonResponse($response);
-    }
-
+	
+	/**
+	 * @return IResponse
+	 */
+	function run( Request $request ) {
+		$appRequest = new ApiApplicationRequest($request);
+		$handler = 'action'.ucfirst($request->getParameter('action'));
+		$response = $this->$handler($appRequest);
+		
+		return new JsonResponse($response);
+	}
+	
 }
